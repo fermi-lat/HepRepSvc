fa272371d4006c50900567a1d27281677060d20b
echarles <>
2008-09-22 22:02:11 +0000

    re-wrote AcdReconFiller to show all the nice acd recon objects

--- src/AcdReconFiller.h
+++ src/AcdReconFiller.h
@@ -6,26 +6,41 @@
 
 #include "Filler.h"
 
-class IGlastDetSvc;
+class IAcdGeometrySvc;
 class IDataProviderSvc;
 class IParticlePropertySvc;
 class HepRepInitSvc;
+
+namespace Event {
+  class AcdRecon;
+  class AcdHit;
+  class AcdTkrGapPoca;
+  class AcdTkrHitPoca;
+  class AcdTkrIntersection;
+  class AcdTkrLocalCoords;
+  class AcdPocaData;
+}
+
+class AcdTileDim;
+class AcdRibbonDim;
+
 
 /** 
  *  @class AcdReconFiller
  *
  *  @brief This is the HepRep fillers for all the ACD relevant
- *  objects (recond and digi). This is a "bookmark" filler waiting for some real
- *  ACD expert to help in developing it
+ *  objects (recond and digi).
+ *
+ *  This filler outlines Acd tiles and ribbons with signals in them.
  *
  *  @author R.Giannitrapani
- */
+ **/
 
 class AcdReconFiller: public Filler{
   
  public:
   AcdReconFiller(HepRepInitSvc* hrisvc,
-                 IGlastDetSvc* gsvc,
+                 IAcdGeometrySvc* gsvc,
                  IDataProviderSvc* dpsvc,
                  IParticlePropertySvc* ppsvc);
   
@@ -35,12 +50,52 @@ class AcdReconFiller: public Filler{
   /// which subinstances to fill
   virtual void fillInstances (std::vector<std::string>&);
 
- private:
+  /// Check to see if a type is in the list of known types
+  bool hasType(std::vector<std::string>& list, std::string type); 
+
+ protected:
+
+  /// Fill the HepReps for the Recons stuff
+  void fillAcdRecon(std::vector<std::string>& typesList);
+
+  /// Fill the HepReps for the AcdHitCol
+  void fillAcdHitCol( const Event::AcdRecon& recon );
+
+  /// Fill the HepReps for the AcdTkrHitPocaCol
+  void fillAcdTkrHitPocaCol( const Event::AcdRecon& recon );
+
+  /// Fill the HepReps for the AcdTkrGapPocaCol
+  void fillAcdTkrGapPocaCol( const Event::AcdRecon& recon );
+  
+  /// Fill the HepReps for the AcdTkrIntersectionCol
+  void fillAcdTkrIntersectionCol( const Event::AcdRecon& recon );
+
+  /// Fill the HepRep for a single AcdHit
+  void addAcdHit( const Event::AcdHit& aHit );
+
+  /// Fill the HepRep for a AcdTkrHitPoca
+  void addAcdTkrHitPoca( const Event::AcdTkrHitPoca& aPoca );  
+
+  /// Fill the HepRep for a AcdTkrGapPoca
+  void addAcdTkrGapPoca( const Event::AcdTkrGapPoca& aPoca );  
+
+  /// Fill the HepRep for AcdTkrLocalCoords
+  void addAcdTkrLocalCoords( const Event::AcdTkrLocalCoords& local );
+
+  /// Fill the HepRep for AcdPocaData
+  void addAcdPocaData( const Event::AcdPocaData& pocaData );
+
+  /// Fill the HepRep for AcdTkrIntersection
+  void addAcdTkrIntersection( const Event::AcdTkrIntersection& inter );
+
+
+private:
+
   HepRepInitSvc* m_hrisvc;
-  IGlastDetSvc* m_gdsvc;
+  IAcdGeometrySvc* m_acdsvc;
   IDataProviderSvc* m_dpsvc;
   IParticlePropertySvc* m_ppsvc;
 
 };
 
-#endif //CALRECONFILLER_H
+#endif // ACDRECONFILLER_H
