f709e2d5a874718bdb64b489a344722a42883bd2
cvs2git <>
2009-01-23 00:06:38 +0000

    This commit was manufactured by cvs2svn to create tag 'v0r29p2'.
    Sprout from master 2008-10-09 06:13:20 UTC lsrea 'AutoUpdate'
    Cherrypick from master 2009-01-23 00:06:35 UTC ecephas 'Check in for GlastRelease-15-49-00':
        HepRepSvcLib.py
        SConscript

--- HepRepSvcLib.py
+++ HepRepSvcLib.py
@@ -1,10 +1,10 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/HepRepSvc/HepRepSvcLib.py,v 1.1 2008/09/02 11:14:25 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['HepRepSvc'])
     env.Tool('CalUtilLib')
     env.Tool('CalibDataLib')
     env.Tool('GlastSvcLib')
-
+    env.Tool('AcdUtilLib')
 def exists(env):
     return 1;
