86ca66ea38964d2a6a47a887ce45b2934b0055af
usher <>
2009-12-14 22:46:23 +0000

    fix crashing when no xtal to cluster relations

--- src/CalReconFiller.h
+++ src/CalReconFiller.h
@@ -15,6 +15,7 @@ class Point;
 class Vector;
 
 #include "Event/Recon/CalRecon/CalCluster.h"
+#include "Event/Recon/CalRecon/CalMipClasses.h" 
 /** 
  *  @class CalReconFiller
  *
@@ -27,66 +28,74 @@ class Vector;
 
 class CalReconFiller: public Filler{
   
- public:
-  CalReconFiller(HepRepInitSvc* hrisvc,
-                 IGlastDetSvc* gsvc,
-                 IDataProviderSvc* dpsvc,
-                 IParticlePropertySvc* ppsvc);
+public:
+    CalReconFiller(HepRepInitSvc* hrisvc,
+                   IGlastDetSvc* gsvc,
+                   IDataProviderSvc* dpsvc,
+                   IParticlePropertySvc* ppsvc);
   
-  /// This method init the type tree
-  virtual void buildTypes ();
-  /// This method fill the instance tree, using the string vector to decide
-  /// which subinstances to fill
-  virtual void fillInstances (std::vector<std::string>&);
+    /// This method init the type tree
+    virtual void buildTypes ();
+    /// This method fill the instance tree, using the string vector to decide
+    /// which subinstances to fill
+    virtual void fillInstances (std::vector<std::string>&);
 
 
- private:
+private:
+    void drawClusters(Event::CalClusterCol* clusters, Event::CalClusterHitTabList* xTal2ClusTabList);
+    void drawCluster(Event::CalCluster* cluster, std::string color = "white");
+    void drawXtals(std::vector<Event::CalClusterHitRel*>& xTalRelVec, std::string color="white");
+    void drawXtals(Event::CalXtalRecCol* xTalCol, std::string color = "white");
+    void drawXtal(Event::CalXtalRecData* xTal, double eMax, std::string color="white");
+    void drawMips(Event::CalMipTrackCol* mipCol);
 
-  void drawXtals(std::vector<Event::CalClusterHitRel*>& xTalRelVec);
+    HepRepInitSvc* m_hrisvc;
+    IGlastDetSvc* m_gdsvc;
+    IDataProviderSvc* m_dpsvc;
+    IParticlePropertySvc* m_ppsvc;
 
-  HepRepInitSvc* m_hrisvc;
-  IGlastDetSvc* m_gdsvc;
-  IDataProviderSvc* m_dpsvc;
-  IParticlePropertySvc* m_ppsvc;
+    /// enums stolen from CalUtil/CalDefs.h (not in "uses" path)
 
-  /// enums stolen from CalUtil/CalDefs.h (not in "uses" path)
+    /** Volume ID field identifiers.  shouldn't be here, but there is no
+        global def as far as i can tell.
+    */
+    enum {fLATObjects, fTowerY, fTowerX, fTowerObjects, fLayer,
+          fMeasure, fCALXtal,fCellCmp, fSegment};
 
-  /** Volume ID field identifiers.  shouldn't be here, but there is no
-      global def as far as i can tell.
-  */
-  enum {fLATObjects, fTowerY, fTowerX, fTowerObjects, fLayer,
-        fMeasure, fCALXtal,fCellCmp, fSegment};
+    std::string* m_colorArray;
+    int          m_maxColors;
+    int          m_colorIndex;
 
-  /// crystal half height
-  float m_xtalHalfHeight;
+    /// crystal half height
+    float m_xtalHalfHeight;
 
-  /// crystal half width
-  float m_xtalHalfWidth;
+    /// crystal half width
+    float m_xtalHalfWidth;
 
-  /// crystal half length
-  float m_xtalHalfLength;
+    /// crystal half length
+    float m_xtalHalfLength;
 
-  /// Z position of top calorimeter layer
-  float m_calZtop;
+    /// Z position of top calorimeter layer
+    float m_calZtop;
   
-  /// Z position of bottom calorimeter layer
-  float m_calZbottom;
-  //-- XML GEOMETRY CONSTANTS --//
+    /// Z position of bottom calorimeter layer
+    float m_calZbottom;
+    //-- XML GEOMETRY CONSTANTS --//
   
-  /// number of x towers
-  int m_xNum;    
-  /// number of y towers
-  int m_yNum;    
-  /// the value of fTowerObject field, defining calorimeter module 
-  int m_eTowerCAL;
-  /// the value of fLATObjects field, defining LAT towers 
-  int m_eLATTowers; 
-  /// the value of fCellCmp field defining CsI crystal
-  int m_eXtal;      
-  /// number of geometric segments per Xtal
-  int m_nCsISeg;
-  /// number of layers
-  int m_nLayers;
+    /// number of x towers
+    int m_xNum;    
+    /// number of y towers
+    int m_yNum;    
+    /// the value of fTowerObject field, defining calorimeter module 
+    int m_eTowerCAL;
+    /// the value of fLATObjects field, defining LAT towers 
+    int m_eLATTowers; 
+    /// the value of fCellCmp field defining CsI crystal
+    int m_eXtal;      
+    /// number of geometric segments per Xtal
+    int m_nCsISeg;
+    /// number of layers
+    int m_nLayers;
 };
 
 #endif //CALRECONFILLER_H
